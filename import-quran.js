ccbab4133528ac317525f1a3d10dee9c8dd48baa
axeemhaider <axeemhaider@gmail.com>
2021-03-20 11:52:21 +0500

    import quran data is complete

--- import-quran.js
+++ import-quran.js
@@ -1,4 +1,7 @@
+const elasticSearch = require("elasticsearch");
 const { Model, Field } = require("fireo");
+
+const esClient = new elasticSearch.Client({ host: "http://localhost:9200" });
 
 String.prototype.allReplace = function (arr) {
   let retStr = this;
@@ -23,6 +26,11 @@ const filterContent = (text) => {
   let content = ayah.content.english.allReplace([
     ",",
     "-",
+    "!",
+    ";",
+    ":",
+    '"',
+    "\\?",
     "\\.",
     "\\[",
     "\\]",
@@ -32,16 +40,29 @@ const filterContent = (text) => {
 };
 
 const main = async () => {
-  const snapshot = await Ayah.collection.fetch(7);
+  const snapshot = await Ayah.collection.fetch();
 
   const elkData = [];
 
   for (ayah of snapshot.list) {
+    // Only pass the first Bismillah
+    if (ayah.number === 0 && ayah.id !== "1-0") {
+      continue;
+    }
+
     elkData.push({ create: { _id: ayah.id } });
     elkData.push({ text: filterContent(ayah.content.english) });
+
+    console.log("surah", ayah.surah_number, "ayah ", ayah.number);
   }
 
-  console.log(elkData);
+  try {
+    console.log("Importing data into ELK...");
+    const result = await esClient.bulk({ index: "quran", body: elkData });
+    console.log("Complete!!!");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 main();
